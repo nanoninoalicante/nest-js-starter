0a53990f44ac4dd19311041cc6d6d90e0de7da9c
Christopher Hill <cjameshill@gmail.com>
2023-07-04 11:56:56 +0200

    Added public and private key

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { HelloModule } from "./hello/hello.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ItemsModule } from './items/items.module';
+import { ConfigModule } from "@nestjs/config";
+import { FasterpayModule } from "./fasterpay/fasterpay.module";
 
 @Module({
     imports: [
@@ -13,15 +12,7 @@ import { ItemsModule } from './items/items.module';
             isGlobal: true,
             cache: false,
         }),
-        MongooseModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: async (configService: ConfigService) => ({
-                uri: configService.get<string>("MONGODB_URI"),
-            }),
-            inject: [ConfigService],
-            connectionName: "primary",
-        }),
-        ItemsModule,
+        FasterpayModule,
     ],
     controllers: [AppController],
     providers: [AppService],
