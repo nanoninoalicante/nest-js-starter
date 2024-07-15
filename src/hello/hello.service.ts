e45e0d2b78a9665bd4d759f6846885b9bb710d75
Christopher Hill <cjameshill@gmail.com>
2024-07-15 15:23:43 +0200

    chore: Update hello world message in app and hello services

--- src/hello/hello.service.ts
+++ src/hello/hello.service.ts
@@ -6,9 +6,14 @@ export class HelloService {
     constructor(protected config: ConfigService) {}
     async getWorld(body: any = {}, params: any = {}) {
         const envExample = this.config.get<string>("ENV_EXAMPLE");
-        return { message: "world! Updated", ...body, ...params, envExample };
+        return {
+            message: "world! Updated on MIG",
+            ...body,
+            ...params,
+            envExample,
+        };
     }
-    async getWorldError(body: any = {}, params: any = {}) {
+    async getWorldError() {
         try {
             throw new Error("testing error response");
         } catch (error) {
